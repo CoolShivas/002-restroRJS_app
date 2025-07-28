1172d8b77d7500a58aec514d8913a913e3ecb3f4
CoolShivas <shivas2710cool00@gmail.com>
2025-07-28 11:08:40 +0530

    Formation of MenuCards  functional component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import MenuCards from "./components/basicThings/MenuCards";
 import AppName from "./components/AppName";
 
 const App = () => {
   return (
     <>
       <AppName></AppName>
+      <MenuCards></MenuCards>
     </>
   );
 };
---
+++ src/components/basicThings/MenuCards.js
@@ -0,0 +1,25 @@
+const MenuCards = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-body">
+            <span className="card-number card-circle subtitle">1</span>
+            <span className="card-author subtitle">Breakfast</span>
+            <h2 className="card-title">Maggie</h2>
+            <span className="card-description subtitle">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              nobis temporibus accusamus libero! Alias tempore sequi quam eaque
+              repudiandae, cum beatae sed eum ut quis!
+            </span>
+            <div className="card-read">Read</div>
+          </div>
+          <img alt="image not found" />
+          <span className="card-tag subtitle">Order Now</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuCards;
