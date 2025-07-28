3febb940479df214f2491aace50af231b7b50e2b
CoolShivas <shivas2710cool00@gmail.com>
2025-07-28 13:06:30 +0530

    Passing the menuApiData dynamically with the help of map method with key identifier also.

--- src/components/basicThings/MenuCards.js
+++ src/components/basicThings/MenuCards.js
@@ -5,27 +5,27 @@ const MenuCards = ({ menuApiData }) => {
 
   return (
     <section className="main-card--container">
-      {menuApiData.map((curr, ind) => {
+      {menuApiData.map((currElement) => {
         return (
-          <>
-            <div className="card-container">
-              <div className="card">
-                <div className="card-body">
-                  <span className="card-number card-circle subtitle">1</span>
-                  <span className="card-author subtitle">Breakfast</span>
-                  <h2 className="card-title">Maggie</h2>
-                  <span className="card-description subtitle">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fugiat nobis temporibus accusamus libero! Alias tempore
-                    sequi quam eaque repudiandae, cum beatae sed eum ut quis!
-                  </span>
-                  <div className="card-read">Read</div>
-                </div>
-                <img alt="image not found" />
-                <span className="card-tag subtitle">Order Now</span>
+          <div className="card-container" key={currElement.id}>
+            <div className="card">
+              <div className="card-body">
+                <span className="card-number card-circle subtitle">
+                  {currElement.id}
+                </span>
+                <span className="card-author subtitle">
+                  {currElement.category}
+                </span>
+                <h2 className="card-title">{currElement.name}</h2>
+                <span className="card-description subtitle">
+                  {currElement.description}
+                </span>
+                <div className="card-read">Read</div>
               </div>
+              <img src={currElement.image} alt="image not found" />
+              <span className="card-tag subtitle">Order Now</span>
             </div>
-          </>
+          </div>
         );
       })}
     </section>
