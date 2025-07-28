5e54a4cf4d07915972d7b92e918c57555a22d959
CoolShivas <shivas2710cool00@gmail.com>
2025-07-28 12:22:22 +0530

    With the help of map method we are looping menuApiData on forming to get multiple cards.

--- src/components/basicThings/MenuCards.js
+++ src/components/basicThings/MenuCards.js
@@ -5,23 +5,29 @@ const MenuCards = ({ menuApiData }) => {
 
   return (
     <>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-body">
-            <span className="card-number card-circle subtitle">1</span>
-            <span className="card-author subtitle">Breakfast</span>
-            <h2 className="card-title">Maggie</h2>
-            <span className="card-description subtitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              nobis temporibus accusamus libero! Alias tempore sequi quam eaque
-              repudiandae, cum beatae sed eum ut quis!
-            </span>
-            <div className="card-read">Read</div>
-          </div>
-          <img alt="image not found" />
-          <span className="card-tag subtitle">Order Now</span>
-        </div>
-      </div>
+      {menuApiData.map((curr, ind) => {
+        return (
+          <>
+            <div className="card-container">
+              <div className="card">
+                <div className="card-body">
+                  <span className="card-number card-circle subtitle">1</span>
+                  <span className="card-author subtitle">Breakfast</span>
+                  <h2 className="card-title">Maggie</h2>
+                  <span className="card-description subtitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugiat nobis temporibus accusamus libero! Alias tempore
+                    sequi quam eaque repudiandae, cum beatae sed eum ut quis!
+                  </span>
+                  <div className="card-read">Read</div>
+                </div>
+                <img alt="image not found" />
+                <span className="card-tag subtitle">Order Now</span>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 };
